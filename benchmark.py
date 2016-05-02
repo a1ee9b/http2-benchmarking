a6080891ff92a48710f6be3315e2b4839fec28e4
Jannik Zappe <jannik.zappe@udo.edu>
2016-05-02 17:41:57 +0200

    Idea files to be ignored

--- benchmark.py
+++ benchmark.py
@@ -139,6 +139,5 @@ if __name__ == "__main__":
     # execute only if run as a script
     runBenchmarks()
     result = analyzeTimings()
-    print("/n", result)
+    print("\n", result)
     printResults()
-
